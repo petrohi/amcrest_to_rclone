cb6d33154399404118c68444291d7dbcdbf8f84c
Peter Hizalev <peter.hizalev@gmail.com>
2026-07-13 21:22:14 -0700

    Upload hour dirs before now minus 2 hours

--- sync_amcrest.py
+++ sync_amcrest.py
@@ -7,6 +7,12 @@ import toml
 import re
 import time
 
+from zoneinfo import ZoneInfo
+from datetime import datetime, timedelta
+
+tzinfo = ZoneInfo("America/Los_Angeles")
+upload_before_timedelta = timedelta(hours=2)
+
 def read_config(config_file_name):
     with open(config_file_name, 'r') as config_file:
         return toml.loads(config_file.read())
@@ -18,9 +24,9 @@ ffmpeg_concat_file = config['daemon']['ffmpeg_concat_file']
 DATE_REGEX = re.compile('([0-9]{4})-([0-9]{2})-([0-9]{2})')
 HOUR_REGEX = re.compile('[0-9]{2}')
 MP4_REGEX = re.compile('.+\\.mp4$')
-UNFINISHED_MP4_REGEX = re.compile('.+\\.mp4_$')
 
 def scan(camera_name, camera_dir, rclone_remote):
+    upload_before_dt = datetime.now(tzinfo) - upload_before_timedelta
     date_dirs = os.listdir(camera_dir)
     for date_dir in date_dirs:
         date_match = DATE_REGEX.match(date_dir)
@@ -32,37 +38,32 @@ def scan(camera_name, camera_dir, rclone_remote):
             hour_dirs = os.listdir(hours_dir)
             for hour_dir in hour_dirs:
                 if HOUR_REGEX.match(hour_dir):
+                    dir_dt = datetime.strptime(date_dir + ' ' + hour_dir, '%Y-%m-%d %H').replace(tzinfo=tzinfo)
                     mp4s_dir = hours_dir + '/' + hour_dir
-                    mp4s = os.listdir(mp4s_dir)
 
-                    if all(not UNFINISHED_MP4_REGEX.match(mp4) for mp4 in mp4s):
-                        print(camera_name + ' confirming: ' + mp4s_dir)
-                        time.sleep(60)
+                    if upload_before_dt > dir_dt:
                         mp4s = os.listdir(mp4s_dir)
                         mp4s.sort()
 
-                        if all(not UNFINISHED_MP4_REGEX.match(mp4) for mp4 in mp4s):
-                            print(camera_name + ' concat: ' + mp4s_dir)
-                            with open(ffmpeg_concat_list_file, "w") as mp4_list_file:
-                                for mp4 in mp4s:
-                                    if MP4_REGEX.match(mp4):
-                                        print(camera_name + ' concat file: ' + mp4)
-                                        mp4_list_file.write('file \'' + mp4s_dir + '/' + mp4 + '\'\n')
+                        print(camera_name + ' concat: ' + mp4s_dir)
+                        with open(ffmpeg_concat_list_file, "w") as mp4_list_file:
+                            for mp4 in mp4s:
+                                if MP4_REGEX.match(mp4):
+                                    print(camera_name + ' concat file: ' + mp4)
+                                    mp4_list_file.write('file \'' + mp4s_dir + '/' + mp4 + '\'\n')
 
-                            os.system('ffmpeg -hide_banner -loglevel error -y -safe 0 -f concat -i /tmp/ffmpeg_concat_list.txt -vcodec copy -acodec copy ' + ffmpeg_concat_file)
-                            os.remove(ffmpeg_concat_list_file)
+                        os.system('ffmpeg -hide_banner -loglevel error -y -safe 0 -f concat -i /tmp/ffmpeg_concat_list.txt -vcodec copy -acodec copy ' + ffmpeg_concat_file)
+                        os.remove(ffmpeg_concat_list_file)
 
-                            if os.path.exists(ffmpeg_concat_file):
-                                rclone_path = rclone_remote  + ':' + camera_name + '/' + year + '/' + month + '/' + day + '/' + hour_dir + '.mp4'
-                                print(camera_name + ' upload: ' + rclone_path)
-                                os.system('rclone copyto ' + ffmpeg_concat_file + ' "' + rclone_path + '"')
-                                os.remove(ffmpeg_concat_file)
-                            else:
-                                print(camera_name + ' bad FFMPEG concat: ' + mp4s_dir)
-
-                            shutil.rmtree(mp4s_dir)
+                        if os.path.exists(ffmpeg_concat_file):
+                            rclone_path = rclone_remote  + ':' + camera_name + '/' + year + '/' + month + '/' + day + '/' + hour_dir + '.mp4'
+                            print(camera_name + ' upload: ' + rclone_path)
+                            os.system('rclone copyto ' + ffmpeg_concat_file + ' "' + rclone_path + '"')
+                            os.remove(ffmpeg_concat_file)
                         else:
-                            print(camera_name + ' unfinished: ' + mp4s_dir)
+                            print(camera_name + ' bad FFMPEG concat: ' + mp4s_dir)
+
+                        shutil.rmtree(mp4s_dir)
                     else:
                         print(camera_name + ' unfinished: ' + mp4s_dir)
 
